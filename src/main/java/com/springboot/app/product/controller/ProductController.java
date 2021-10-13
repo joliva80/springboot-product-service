58ff873074be7785b25a09f94acefa9c0d7e5010
Joan Oliva <joanoj@gmail.com>
2021-10-13 19:48:49 +0200

    config service

--- src/main/java/com/springboot/app/product/controller/ProductController.java
+++ src/main/java/com/springboot/app/product/controller/ProductController.java
@@ -9,11 +9,13 @@ import com.springboot.app.product.model.service.IProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ProductController {
     
     @Autowired
